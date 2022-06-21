68fa2802fb5a16322ced8293a07ab5fe0a58f9a5
Sanyasee Sahu <35070467+sanyasee17@users.noreply.github.com>
2022-06-21 09:42:32 +0530

    Add files via upload

---
+++ src/com/sanyasee/searching/binary/assignments/GuessNumber.java
@@ -0,0 +1,25 @@
+package com.sanyasee.searching.binary.assignments;
+
+// https://leetcode.com/problems/guess-number-higher-or-lower/
+
+public class GuessNumber {
+    public static void main(String[] args) {
+
+    }
+
+    public int guessNumber(int n) {
+        int start = 1;
+        int end = n;
+        while(start < end) {
+            int mid = start + (end - start) / 2;
+            if(guess(mid) == 0) {
+                return mid;
+            } else if(guess(mid) == 1) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return start;
+    }
+}
